91d5890996e8adbe4e25cdfb522bac26f583dd29
Calvin Rong <chrong@ucsc.edu>
2020-07-15 15:28:15 -0700

    Basic files and structure

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// Create game config object
+let config = {
+    type: Phaser.Canvas,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ],
+};
+
+// create main game object
+let game = new Phaser.Game(config);
+
+// define game settings
+game.settings = {
+    //
+}
